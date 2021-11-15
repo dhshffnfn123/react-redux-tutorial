d9f58043dc2693e1956d15af2e3cc56b037d5fb8
B H An <anbyunghoon0107@gamil.com>
2021-11-15 22:29:27 +0900

    CounterContainer 생성완료

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
@@ -9,32 +10,34 @@ const CounterContainer = ({ number, increase, decrease }) => {
     );
 };
 
-// mapStateToProps와 mapDispatchToProps에서 반환하는 객체 내부의 값들은 컴포넌트의 props로 전달된다.
-const mapStateToProps = state => ({ // state를 파라미터로 받아온다. (현재 store가 지니고 있는 상태)
-    number : state.counter.number,
-});
-const mapDispatchToProps = dispatch => ({   // store 내장함수 dispatch를 받아온다.
-    // 임시 함수
-    increase : () => {
-        dispatch(increase());
-    },
-    decrease : () => {
-        dispatch(decrease());
-    },
-});
 
-//connect 함수 내부에 익명 함수 형태로 선언해도 문제가 되지 않는다.
+// connect 함수 내부에 익명 함수 형태로 선언해도 문제가 되지 않는다.
+// mapDispatchToProps에 해당하는 파라미터를 함수 형태가 아닌 액션 생성 함수로 이루어진 객체 형태로 넣어주어도 된다.
+export default connect(
+    state => ({
+        number : state.counter.number,
+    }),
+    {
+        increase,
+        decrease,
+    },
+)(CounterContainer);
+// 위와 같이 두 번째 파라미터를 아예 객체 형태로 넣어 주면 connect 함수가 내부적으로 bindActionCreators 작업을 대신 해준다.
+    
+    
 /* export default connect(
     state => ({
         number : state.counter.number,
     }),
-    dispatch => ({
-        increase : () => dispatch(increase()),
-        decrease : () => dispatch(decrease()),
-    }),
-)(CounterContainer) */
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(CounterContainer);
+    dispatch => 
+    bindActionCreators(
+        {
+            increase,
+            decrease,
+        },
+        dispatch,
+    ),
+    )(CounterContainer);
+    
+    
+     */
