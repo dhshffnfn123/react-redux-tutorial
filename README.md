6616b3dc5dfe21c2db8304cf6b626313426e20ca
B H An <anbyunghoon0107@gamil.com>
2021-11-18 12:42:34 +0900

    immer 사용해보기

--- README.md
+++ README.md
@@ -26,3 +26,8 @@ ___
 > - connect 함수를 호출하고 나면 또 다른 함수를 반환한다.
 > - 반환된 함수에 컴포넌트를 파라미터로 넣어주면 리덕스와 연동된 컴포넌트가 만들어진다.
 
+## useSelector
+> + useSelector Hook을 이용하면 connect 함수를 사용하지 않고도 리덕스의 상태를 조회할 수 있다.
+> - 사용법 : const 결과 = useSelector(상태 선택 함수);
+>   - 위의 __상태 선택 함수__ 는 mapStateToProps와 형태가 같다. 
+
--- src/modules/todos.js
+++ src/modules/todos.js
@@ -39,21 +39,24 @@ const initialState = {
 
 const todos = handleActions(
     {
-        [CHANGE_INPUT]: (state, { payload: input }) => ({...state, input}), // payload: input => payload의 이름을 input으로 설정해준다.
-        [INSERT]: (state, { payload: todo }) => ({
-            ...state,
-            todos: state.todos.concat(todo),
-        }),
-        [TOGGLE]: (state, { payload: id }) => ({
-            ...state,
-            todos: state.todos.map(todo => 
-                todo.id === id ? { ...todo, done: !todo.done } : todo,
-            ),
-        }),
-        [REMOVE]: (state, { payload: id }) => ({
-            ...state,
-            todos: state.todos.filter(todo => todo.id !== id),
-        }),
+        [CHANGE_INPUT]: (state, { payload: input }) => 
+            produce(state, draft => {
+                draft.input = input;
+            }),
+        [INSERT]: (state, { payload: todo }) =>
+            produce(state, draft => {
+                draft.todos.push(todo);
+            }),
+        [TOGGLE]: (state, { payload: id }) => 
+            produce(state, draft => {
+                const todo = draft.todos.find(todo => todo.id === id);
+                todo.done = !todo.done;
+            }),
+        [REMOVE]: (state, { payload: id }) => 
+            produce(state, draft => {
+                const index = draft.todos.findIndex(todo => todo.id === id);
+                draft.todos.splice(index, 1);
+            })
     },
     initialState,
 );
